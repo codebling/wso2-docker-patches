4963188b6b28bd2aa536043e763a62ba79e8ca59
Code Bling <codeblingx@gmail.com>
2021-12-15 20:10:58 -0500

    Move Dockerfile to os directectory

--- build-and-push-all-tags.sh
+++ build-and-push-all-tags.sh
@@ -5,4 +5,4 @@ UBUNTU_TAGS=$(for t in `echo "$TAGS" | grep -v -e centos -e alpine`; do echo $t;
 ALPINE_TAGS=$(for t in `echo "$TAGS" | grep alpine`; do echo $t; done); 
 CENTOS_TAGS=$(for t in `echo "$TAGS" | grep centos`; do echo $t; done);
 
-for t in `echo "$UBUNTU_TAGS"`; do docker build --build-arg=IMAGE=wso2/wso2is:$t . -t codebling/wso2is:$t && docker push codebling/wso2is:$t; done
+for t in `echo "$UBUNTU_TAGS"`; do docker build --build-arg=IMAGE=wso2/wso2is:$t ./ubuntu -t codebling/wso2is:$t && docker push codebling/wso2is:$t; done
