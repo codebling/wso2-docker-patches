325eaa64401c390a80c8fc5c3a336206e94da8f0
Code Bling <codeblingx@gmail.com>
2021-12-15 20:01:41 -0500

    Add script to build and push all tags

---
+++ build-and-push-all-tags.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+TAGS=`curl -L -s 'https://registry.hub.docker.com/v2/repositories/wso2/wso2is/tags/' | jq --raw-output '.results[].name'`; 
+UBUNTU_TAGS=$(for t in `echo "$TAGS" | grep -v -e centos -e alpine`; do echo $t; done); 
+ALPINE_TAGS=$(for t in `echo "$TAGS" | grep alpine`; do echo $t; done); 
+CENTOS_TAGS=$(for t in `echo "$TAGS" | grep centos`; do echo $t; done);
+
+for t in `echo "$UBUNTU_TAGS"`; do docker build --build-arg=IMAGE=wso2/wso2is:$t . -t codebling/wso2is:$t && docker push codebling/wso2is:$t; done
